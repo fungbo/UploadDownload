759b6a373377513b9d94c72ca3837df0bf9b9461
bfeng <bfeng@thoughtworks.com>
2015-10-12 11:31:53 +0800

    Make downloading support chinese file name

--- src/main/java/com/tw/FileController.java
+++ src/main/java/com/tw/FileController.java
@@ -14,6 +14,7 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @Controller
 public class FileController {
@@ -29,7 +30,8 @@ public class FileController {
     public FileSystemResource download(HttpServletResponse response) throws IOException {
         String fileName = "test.jar";
         String filePath = "/Users/bfeng/H5/" + fileName;
-        response.setHeader("Content-disposition", "attachment; filename=" + fileName);
+        String contentDisposition = "attachment; filename*=UTF-8''" + URLEncoder.encode("知乎", "UTF-8");
+        response.setHeader("Content-disposition", contentDisposition);
         return new FileSystemResource(filePath);
     }
 
